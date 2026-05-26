16793b267b7427b558dc42991bff0482a9cc937e
arBusto <agustin2005bascouer@gmail.com>
2026-05-25 22:32:13 -0300

    Clase empresa
    70 72 6f 66 65 67 6F 72 64 6F 70 65 6C 6F 74 75 64 6F

---
+++ src/principal/Empresa.java
@@ -0,0 +1,43 @@
+package principal;
+
+import java.util.HashSet;
+
+public class Empresa {
+    
+    // Datos
+    private String cuit;
+    private String nombreFantasia;
+    private String telefono;
+    private String email;
+    private String nombreContacto;
+    
+    // Colección para búsquedas rápidas (O(1)) de autorizaciones
+    private HashSet<String> dnisAutorizados; 
+
+    // Operaciones (Constructor completo)
+    public Empresa(String cuit, String nombreFantasia, String telefono, String email, String nombreContacto) {
+        this.cuit = cuit;
+        this.nombreFantasia = nombreFantasia;
+        this.telefono = telefono;
+        this.email = email;
+        this.nombreContacto = nombreContacto;
+        
+        // Inicializamos el HashSet vacío para poder agregar DNIs luego sin que dé error
+        this.dnisAutorizados = new HashSet<>();
+    }
+
+    // Agrega el DNI de un usuario a la lista de autorizados
+    public void agregarUsuarioAutorizado(String dni) {
+        
+    }
+
+    // Verifica en O(1) si un DNI está dentro del HashSet de autorizados
+    public boolean estaAutorizado(String dni) {
+        return false;
+    }
+
+    // Devuelve el identificador de la empresa
+    public String obtenerCuit() {
+        return null;
+    }
+}
